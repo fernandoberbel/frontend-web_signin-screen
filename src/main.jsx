0c75d6cc506f9d449703a2aceb7eff29128d1d26
fernandoberbel <fberbel1000@gmail.com>
2024-03-29 01:02:44 -0300

    feat: add theme and first component

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { SignIn } from "./screens/signin";
 
+import "./global.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SignIn />
